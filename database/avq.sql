05d2025407909f64a4ecbeb746d4b2d010ba32c7
Ray Vincent Phillip D. Villaver <phillip60341@gmail.com>
2016-10-11 00:07:27 +0800

    DATABASE UPDATES

--- database/avq.sql
+++ database/avq.sql
@@ -31,7 +31,7 @@ CREATE TABLE `dental_records` (
   `description` longtext NOT NULL,
   `date_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `dental_records` (
 
 LOCK TABLES `dental_records` WRITE;
 /*!40000 ALTER TABLE `dental_records` DISABLE KEYS */;
-INSERT INTO `dental_records` VALUES (1,1,1,0,NULL,'try lang','2016-09-19 17:49:10'),(2,5,1,3,NULL,'dded','2016-09-19 17:51:27');
+INSERT INTO `dental_records` VALUES (1,1,1,0,NULL,'try lang','2016-09-19 17:49:10'),(2,5,1,3,NULL,'dded','2016-09-19 17:51:27'),(3,1,4,0,NULL,'tinry ko today','2016-09-25 23:05:25'),(4,2,1,0,NULL,'restoration for 2','2016-10-08 02:07:52'),(5,1,2,0,NULL,'packeck kay sir nung oct 10','2016-10-10 18:50:19');
 /*!40000 ALTER TABLE `dental_records` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,7 +88,7 @@ CREATE TABLE `patient` (
   `status` varchar(15) NOT NULL,
   `occupation` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,7 +97,7 @@ CREATE TABLE `patient` (
 
 LOCK TABLES `patient` WRITE;
 /*!40000 ALTER TABLE `patient` DISABLE KEYS */;
-INSERT INTO `patient` VALUES (1,'Ray Vincent Phillip','Dineros','Villaver','Makati City','09480676172','1995-06-03','Single','Web Developer (Backend)'),(2,'Kathleen Claire','De Leon','Saquilayan','Pateros City','09074946945','1995-01-06','Single','Flight Attendant'),(5,'FirstEditRheaMarieGulo','Middle','Last','Address','Contact','1567-01-03','Single','OOOccupation'),(7,'John','Hernandez','Villete','Taguig','12345678','1995-01-01','Single','Occupation'),(9,'First','MIddleEdit','Last','Address','09234435467','0000-00-00','Single','Occupation'),(10,'First','MIddletyng isa','Last','Adress','09234435467','0000-00-00','Single','Occupation'),(11,'AgainFirst1234567','AgainMiddle12345678','AgainLast','AgainAddress','3242314','0000-00-00','Somthing','Occupation'),(12,'John Paulo','Vergara','Cruz','South Cembo','12345678','0000-00-00','Taken','Developer'),(13,'askjh','kjh','lkjh','klj','hklj','0000-00-00','kjlhkj','kjhlkj');
+INSERT INTO `patient` VALUES (1,'Ray Vincent Phillip','Dineros','Villaver','Makati City','09480676172','1995-06-03','Singleddd','Web Developer (Backend)'),(2,'Kathleen Claire','De Leon','Saquilayan','Pateros City','09074946945','1995-01-06','Single','Flight Attendant'),(5,'FirstEditRheaMarieGulo','MiddleUPDATE','Lastxxx','Address','Contact','1567-01-03','Single','pation'),(7,'John','Hernandez','Villete','Taguig','12345678','1995-01-01','Single','manager'),(9,'First','MIddleEdit','Last','Address','09234435467','0000-00-00','Single','Occupation'),(10,'First','MIddletyng isa','Last','Adress','09234435467','0000-00-00','Single','Occupation'),(11,'AgainFirst1234567','AgainMiddle12345678','AgainLast','AgainAddress','3242314','0000-00-00','Single','Occupation'),(12,'John Paulo','Vergara','Cruz','South Cembo','12345678','0000-00-00','Taken','Developer'),(13,'askjh','kjh','lkjh','klj','hklj','0000-00-00','kjlhkj','kjhlkj'),(14,'Aldwin','Balagtas','Labrador','Caloocan','123456789','0000-00-00','Married','Full Stack Developer'),(15,'Michael','Dineros','Quimson','Valle Verde 5','1239879012','0000-00-00','Single','Dota Player'),(16,'Modal First','Modal Middle','Modal Last','Modal Address','Modal Contact','0000-00-00','Modal Status','Modal Occupation'),(17,'Raphael John','Dineros','','','','0000-00-00','',''),(18,'Raphael John','Dineros','Quimson','','','0000-00-00','',''),(19,'Raphael John','Dineros','Quimson','','','0000-00-00','',''),(20,'Raphael John ','Dineros','Quimson','Pasig','09989852484','0000-00-00','Single','Tambay');
 /*!40000 ALTER TABLE `patient` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -129,31 +129,6 @@ LOCK TABLES `payments` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `sample`
---
-
-DROP TABLE IF EXISTS `sample`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sample` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(75) NOT NULL,
-  `password` varchar(75) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `sample`
---
-
-LOCK TABLES `sample` WRITE;
-/*!40000 ALTER TABLE `sample` DISABLE KEYS */;
-INSERT INTO `sample` VALUES (2,'kath','saquilayan'),(3,'pelp','villaver');
-/*!40000 ALTER TABLE `sample` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `services`
 --
 
@@ -178,6 +153,35 @@ LOCK TABLES `services` WRITE;
 INSERT INTO `services` VALUES (1,'Restoration','Pasta',1500),(2,'Oral Prophylaxis','Cleaning',500),(3,'Extraction',NULL,700),(4,'Endodontic Treatment',NULL,800),(5,'Prosthodontic',NULL,500);
 /*!40000 ALTER TABLE `services` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(75) NOT NULL,
+  `password` varchar(75) NOT NULL,
+  `first_name` varchar(200) NOT NULL,
+  `middle_name` varchar(200) NOT NULL,
+  `last_name` varchar(200) NOT NULL,
+  `position` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (2,'kath','saquilayan','De Leon','De Leon','Saquilayan','Managerxxxxxxx'),(3,'pelp','villaver','Ray Vincent Phillip','Dineros','Villaver','Developer');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -188,4 +192,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-09-19 22:27:11
+-- Dump completed on 2016-10-11  0:06:34
